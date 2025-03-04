ae4b39f31ad6ed60ed5c21fc71b68a01e390da59
Jorabek1124 <83777256+Jorabek1124@users.noreply.github.com>
2025-03-04 11:08:01 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,3 @@
-// import { gcd } from './gcd.js';
-// import { modInverse } from './modInverse.js';
-
-
-// document.getElementById("gcdBtn").addEventListener("click", function() {
-//     let a = parseInt(document.getElementById("num1").value);
-//     let b = parseInt(document.getElementById("num2").value);
-
-//     if (isNaN(a) || isNaN(b)) {
-//         document.getElementById("result").innerText = "Iltimos, sonlarni kiriting!";
-//         return;
-//     }
-
-//     let result = gcd(a, b);
-//     document.getElementById("result").innerText = `EKUB(${a}, ${b}) = ${result}`;
-// });
-
-
-// document.getElementById("modInvBtn").addEventListener("click", function() {
-//     let a = parseInt(document.getElementById("num1").value);
-//     let m = parseInt(document.getElementById("num2").value);
-
-//     if (isNaN(a) || isNaN(m)) {
-//         document.getElementById("result").innerText = "Iltimos, sonlarni kiriting!";
-//         return;
-//     }
-
-//     let result = modInverse(a, m);
-//     document.getElementById("result").innerText = `Teskari element: ${result}`;
-// });
 
 import { gcd } from './gcd.js';
 import { modInverse } from './modInverse.js';
